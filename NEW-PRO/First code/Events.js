531aa6780e23b292b5b586d90bde74ab20dc26d5
Pavan56738 <pavanrock859@gmail.com>
2025-08-13 19:53:54 +0530

    Add files via upload

---
+++ NEW-PRO/First code/Events.js
@@ -0,0 +1,12 @@
+// let btn1 = document.querySelector("#btn1");
+// btn1.onclick = () =>{
+//     console.log("button was clicked");
+//     let a =25;
+//     a++;
+//     console.log(a);
+// };
+
+// let div = document.querySelector("div");
+// div.onmouseover = () =>{
+//     console.log("ur inside the div");
+// } 
---
+++ NEW-PRO/First code/loops.js
@@ -0,0 +1,78 @@
+// let sum=0;
+// for(let i=0;i<=6;i++){
+//     sum=sum+i; 
+// }
+//  console.log(sum);
+
+// let i=0;
+// do{
+//     console.log(i);
+//      i++;
+// }while(i<=10);
+
+
+// let str = "apna college"
+// let size = 0;
+// for(let i of str){
+//     console.log("i=",i);
+//     size++;
+// }
+
+// console.log("string size = ", size);
+
+// let student = {
+//     age :43,
+//     cgpa:3.3,
+//     isPass : true
+
+// };
+// for(let key in student){
+//     console.log("key =",key , "value = ",student[key]);
+// }
+
+
+// let num = 0;
+// for(let num=0; num<=100 ; num++){
+//     if(num%2==1){
+//         console.log(num);
+//     }
+//     }
+
+// let guessNum = 25;
+// let userNum = prompt("Guess a number: ");
+// while(guessNum != userNum){
+//     userNum = prompt("you entered the wrong num, Try again");
+// }
+
+// console.log("Congratulations U entered crct num");
+
+
+//stings
+
+
+// let str = "pavan";
+// let str2 = "ram";
+// console.log(str[2]);
+
+// let str = "pavan";
+// str = str.toLowerCase();
+// console.log(str);
+
+// let str = "pavan";
+// console.log(str.slice(0, 3));
+
+
+// let str1 = "pavan";
+// let str2 = " kalyan";
+// console.log(str1.concat(str2));
+
+// let fullName = prompt("enter your full name");
+// let username = "@" + fullName + fullName.length;
+
+// console.log(username);
+
+
+let str = "pavan";
+for(let i of str){
+    console.log(i);
+}
